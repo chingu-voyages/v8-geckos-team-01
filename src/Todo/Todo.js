17a93f11634c6265579d97cda0934b2fe496ac1e
joye <shonubij@gmail.com>
2019-03-30 09:51:57 +0100

    added todo component and route

---
+++ src/Todo/Todo.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+
+class Todo extends Component{
+
+    render(){
+        return(<div> <h1> Todo App Running Live </h1></div>)
+    }
+}
+
+
+export default Todo
+
--- src/Tools.js
+++ src/Tools.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
+import  {BrowserRouter , Route, Link, Switch} from "react-router-dom";
+
+import Todo from './Todo/Todo'
 
 class Tools extends Component {
     render() {
         return (
             <div>
+            <div>
                 <h2>Productivity Tools</h2>
                 <p>Click any of the items below to start being productive:</p>
                 <ol>
                     <li>Pomodoro Clock</li>
-                    <li>To Do List</li>
+                    <li><Link exact to="/todo">To Do List</Link></li>
                     <li>Task Timer</li>
                     <li>Coming soon...</li>
                     <li>Also coming soon...</li>
                 </ol>
             </div>
+
+            
+            <BrowserRouter>
+            <Switch>
+            <Route exact path = "/todo" Component = {Todo} />
+            
+
+            </Switch>
+            </BrowserRouter>
+            
+            </div>
+           
         );
     }
 }
+
 
 export default Tools;
