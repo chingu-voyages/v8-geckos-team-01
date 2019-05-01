30aa39201091f9d78333ae5119a2ee5290fd6e25
Helon <rhelon@gmail.com>
2019-05-01 11:56:12 +0200

    Updates

---
+++ precache-manifest.51f35dd33664888cc5592a56c10d6f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c64d675ae87074732ae",
+    "url": "/v8-geckos-team-01/static/js/runtime~main.e628ae9e.js"
+  },
+  {
+    "revision": "eefb399cb56397349520",
+    "url": "/v8-geckos-team-01/static/js/main.2e319e7a.chunk.js"
+  },
+  {
+    "revision": "b6916d6bcd303fc72056",
+    "url": "/v8-geckos-team-01/static/js/2.dc2568f3.chunk.js"
+  },
+  {
+    "revision": "eefb399cb56397349520",
+    "url": "/v8-geckos-team-01/static/css/main.86d1b88b.chunk.css"
+  },
+  {
+    "revision": "daab1b384dd03c95f93157ec6448e918",
+    "url": "/v8-geckos-team-01/index.html"
+  }
+];
